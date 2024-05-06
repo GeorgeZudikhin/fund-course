b2f2261b926fc73b7a7d86e80545fbef8514f2e7
GeorgeZudikhin <yehorzudikhin17@gmail.com>
2024-05-06 22:10:52 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [posts, setPosts] = useState([
+        {id: 1, title: 'JavaScript', body: 'Description'},
+        {id: 2, title: 'JavaScript', body: 'Description'},
+        {id: 3, title: 'JavaScript', body: 'Description'},
+    ]);
+
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        const newPost = {
+            id: Date.now(),
+            title,
+            body
+        }
+        setPosts([...posts, newPost])
+    }
+
+    return (
+        <div className="App">
+            <form action="">
+                <MyInput
+                    value={title}
+                    onChange={({target: {value}}) => setTitle(value)}
+                    type="text"
+                    placeholder="Post name"
+                />
+                <MyInput
+                    value={body}
+                    onChange={({target: {value}}) => setBody(value)}
+                    type="text"
+                    placeholder="Post description"
+                />
+                <MyButton onClick={addNewPost}>Create post</MyButton>
+            </form>
+            <PostList posts={posts} title={"Posts about JS"}/>
+        </div>
+    );
 }
 
 export default App;
